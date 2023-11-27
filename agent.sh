4c6186e4533a48c77bbf72a27b3490cb65858d0f
zhj15265123 <4850007@gmail.com>
2023-11-27 08:43:13 +0800

    更新 agent.sh

--- agent.sh
+++ agent.sh
@@ -236,7 +236,7 @@ fi
 
 cd /opt
 
-download "https://raw.githubusercontent.com/zhj15265123/cdnfly-kaixin/main/cdnfly/$tar_gz_name" "https://raw.githubusercontent.com/zhj15265123/cdnfly-kaixin/main/cdnfly/$tar_gz_name" "$tar_gz_name"
+download "https://gitee.com/a15265123/cdnfly-kaixin/raw/main/cdnfly/$tar_gz_name" "https://gitee.com/a15265123/cdnfly-kaixin/raw/main/cdnfly/$tar_gz_name" "$tar_gz_name"
 
 rm -rf $dir_name
 tar xf $tar_gz_name
